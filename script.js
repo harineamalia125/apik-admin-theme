30be442ac32e5c7aa3b26ceffb2bd8e3145500b4
harineamalia125 <harineamalia125@gmail.com>
2022-10-03 18:26:19 +0700

    menambah form view dan table view

---
+++ script.js
@@ -0,0 +1,12 @@
+function formatDoc(cmd, value=null) {
+	if(value) {
+		document.execCommand(cmd, false, value);
+	} else {
+		document.execCommand(cmd);
+	}
+}
+
+function addLink() {
+	const url = prompt('Insert url');
+	formatDoc('createLink', url);
+}
